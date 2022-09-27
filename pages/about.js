44657d6145c52fb8a65a4ce836d6a8fb38cdce19
MINH_VN <tqminh@b-create.jp>
2022-09-27 17:08:20 +0700

    update

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const about = props => {
+  return (
+    <div>
+      this is about
+    </div>
+  );
+};
+
+about.propTypes = {
+  
+};
+
+export default about;
